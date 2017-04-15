7d989d98a7f688327967e8bf19da7403a40a8d42
Varun Mehta <varun@mehtasw.com>
2017-04-15 00:39:27 -0400

    more fiddling with temps and threads

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 from flask import Flask
+import threading
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
-if __name__ == '__main__':
+def server():
     app.run(host='0.0.0.0', port=80)
+
+if __name__ == '__main__':
+    serverThread = threading.Thread(target=server)
+    serverThread.start()
